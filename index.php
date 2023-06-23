1bdf044e1274bb1a8d254d647def89ec5a10e0bb
Aensolgon <leha.nov98@mail.ru>
2023-06-23 15:29:10 +0300

    bug fix

--- index.php
+++ index.php
@@ -7,8 +7,8 @@ use app\Config\Application;
 require_once 'config.php';
 require_once 'db/Contracts/IDatabase.php';
 require_once 'db/MySQLDatabase.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/app/helpers/redirect.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/app/Config/Application.php';
+require_once 'app/Helpers/redirect.php';
+require_once 'app/Config/Application.php';
 
 $application = new Application();
 $application::run();
