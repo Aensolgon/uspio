0319e7dc51dd0847b6fee4818ffbbf8a661ec65d
Aensolgon <leha.nov98@mail.ru>
2023-06-23 14:46:50 +0300

    edit config

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 const API_KEY = 'AIzaSyCydTogmH1VAliYbpSsihYysXp41AGCso8';
-const DB_HOST = 'localhost';
-const DB_USERNAME = 'root';
-const DB_PASSWORD = '';
-const DB_NAME = 'uspio';
+const DB_HOST = 'us-cdbr-east-06.cleardb.net';
+const DB_USERNAME = 'b33957dafabbd5';
+const DB_PASSWORD = '5b77b3a8';
+const DB_NAME = 'heroku_29e6381460c4bb0';
 
