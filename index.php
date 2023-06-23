c826d6986d40f6516c04df9962f35dd5f1ca0957
Aensolgon <leha.nov98@mail.ru>
2023-06-23 15:49:08 +0300

    bug fix

--- index.php
+++ index.php
@@ -9,6 +9,6 @@ require_once 'db/Contracts/IDatabase.php';
 require_once 'db/MySQLDatabase.php';
 require_once 'app/Helpers/redirect.php';
 require_once 'app/Config/Application.php';
-
+var_dump($_SERVER['DOCUMENT_ROOT']);die();
 $application = new Application();
 $application::run();
