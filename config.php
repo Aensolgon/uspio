cb8c048a071c0c532173e9a4b89dbb66b686e943
Aensolgon <leha.nov98@mail.ru>
2023-06-23 20:12:23 +0300

    bug fix

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-const API_KEY = 'AIzaSyCydTogmH1VAliYbpSsihYysXp41AGCso8';
-const DB_HOST = 'us-cdbr-east-06.cleardb.net';
-const DB_USERNAME = 'b33957dafabbd5';
-const DB_PASSWORD = '5b77b3a8';
-const DB_NAME = 'heroku_29e6381460c4bb0';
-
